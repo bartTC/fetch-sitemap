d6f3761c3ff79c9ec35543c79fc60a5393ae491c
Martin Mahner <martin@mahner.org>
2024-04-24 09:37:40 +0200

    Release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+# Run tests before release
+./test.sh
+
+poetry version major
+git add pyproject.toml && git commit -am "Bump up version number to v$(poetry version -s)}"
+git tag "v$(poetry version -s)"
+git push --tags
+poetry build
+peotry publish
