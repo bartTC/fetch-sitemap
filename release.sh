d0ddabdd1b2dbde8cecd215a18b3674939fba624
Martin Mahner <martin@mahner.org>
2024-04-24 21:59:19 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -8,6 +8,7 @@ set -o pipefail
 poetry version major
 git add pyproject.toml && git commit -am "Bump up version number to v$(poetry version -s)"
 git tag "v$(poetry version -s)"
+git push
 git push --tags
 poetry build
 poetry publish
