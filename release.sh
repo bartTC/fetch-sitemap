db07ace4babbfcd0ea134cd73f3c2ba19ef97b46
Martin Mahner <martin@mahner.org>
2024-05-10 05:46:03 +0200

    Release cleanpu

--- release.sh
+++ release.sh
@@ -5,12 +5,16 @@ set -o pipefail
 # Run tests before release
 ./tests.sh
 
+# Bump up major version number
 poetry version major
-git add pyproject.toml && git commit -am "Bump up version number to v$(poetry version -s)"
 VERSION="v$(poetry version -s)"
+
+# Commit new version number and create a tag and Github release
+git add pyproject.toml && git commit -am "Bump up version number to v$(poetry version -s)"
 git tag $VERSION
 git push
 git push --tags
-gh release create --generate-notes --latest --title=v24 v24
-poetry build
-poetry publish
+gh release create --generate-notes --latest --title=$VERSION $VERSION
+
+# Publish to Pypi
+poetry publish --build
