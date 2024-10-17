b5dd753780573bef4caee81a167909e7e5e2bd44
Martin Mahner <martin@mahner.org>
2024-10-17 21:57:46 +0200

    Dont sign tags

--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ VERSION="v$(poetry version -s)"
 
 # Commit new version number and create a tag and Github release
 git add pyproject.toml && git commit -am "Bump up version number to v$(poetry version -s)"
-git tag -s $VERSION
+git tag $VERSION
 git push
 git push --tags
 gh release create --generate-notes --latest --title=$VERSION $VERSION
